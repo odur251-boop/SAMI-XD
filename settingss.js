b69c5fb2287162dcd1404faa8a359bdf91ae69fe
odur251-boop <odur251@gmail.com>
2025-08-29 19:06:36 +0300

    Update settingss.js

--- settingss.js
+++ settingss.js
@@ -1,8 +1,8 @@
 
 module.exports = {
-  SESSION_ID: "",  // add your Session Id here
+  SESSION_ID: "malvin~BqlDgTrY#5ZChy7cLCL7kW9CVSUgd1C0UxoPNQrn-6drxeH36zQY",  // add your Session Id here
   
-  OWNER_NUMBER: "263714757857", // put your phone number here
+  OWNER_NUMBER: "256766104887", // put your phone number here
   
   PREFIX: ".", // prefix (e.g., ., /, !, *)
   
